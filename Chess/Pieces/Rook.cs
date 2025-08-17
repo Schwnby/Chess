7af275a35137584df208fff3bc9584f6e797deab
Schwaneby <simonunger@bluewin.ch>
2025-08-17 16:47:36 +0200

    Implemented Rook GetMoves method

--- Chess/Pieces/Rook.cs
+++ Chess/Pieces/Rook.cs
@@ -4,6 +4,41 @@ internal sealed class Rook(PieceColor color) : Piece(color)
 {
     private protected override IEnumerable<Position> GetMoves(Position currentPosition, Board board)
     {
-        throw new NotImplementedException();
+       var movesUp = GetStraightMoves(0, 1, currentPosition, board);
+       var movesRight = GetStraightMoves(1, 0, currentPosition, board);
+       var movesDown = GetStraightMoves(0, -1, currentPosition, board);
+       var movesLeft = GetStraightMoves(-1, 0, currentPosition, board);
+
+        return [..movesUp, ..movesDown, ..movesLeft, ..movesRight];
+    }
+
+    private List<Position> GetStraightMoves(int rankOffset, int fileOffset, Position position, Board board)
+    {
+        var validMoves = new List<Position>();
+        
+        while (true)
+        {
+            position = new Position(position.Rank + rankOffset, position.File + fileOffset);
+            if (position.IsInbound() is false)
+            {
+                break;
+            }
+            
+            var piece = board.GetPiece(position);
+            if (piece is null)
+            {
+                validMoves.Add(position);
+                continue;
+            }
+
+            if (piece.Color != Color)
+            {
+                validMoves.Add(position);
+            }
+            
+            break;
+        }
+        
+        return validMoves;
     }
 }
