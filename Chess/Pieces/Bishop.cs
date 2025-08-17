479549d0665908e433c8da30aa6b3c61c2ad42f6
Schwaneby <simonunger@bluewin.ch>
2025-08-17 19:14:03 +0200

    Implemented Bishop GetMoves method

--- Chess/Pieces/Bishop.cs
+++ Chess/Pieces/Bishop.cs
@@ -4,6 +4,41 @@ internal sealed class Bishop(PieceColor color) : Piece(color)
 {
     private protected override IEnumerable<Position> GetMoves(Position currentPosition, Board board)
     {
-        throw new NotImplementedException();
+        var movesUpLeft = GetDiagonalMoves(-1, -1, currentPosition, board);
+        var movesUpRight = GetDiagonalMoves(1, -1, currentPosition, board);
+        var movesDownLeft = GetDiagonalMoves(-1, 1, currentPosition, board);
+        var movesDownRight = GetDiagonalMoves(1, 1, currentPosition, board);
+
+        return [..movesUpLeft, ..movesUpRight, ..movesDownLeft, ..movesDownRight];
+    }
+
+    private List<Position> GetDiagonalMoves(int rankOffset, int fileOffset, Position position, Board board)
+    {
+        var validMoves = new List<Position>();
+        
+        while (true)
+        {
+            position = new Position(position.Rank + rankOffset, position.File + fileOffset);
+            if (position.IsInbound() is false)
+            {
+                break;
+            }
+            
+            var piece = board.GetPiece(position);
+            if (piece is null)
+            {
+                validMoves.Add(position);
+                continue;
+            }
+
+            if (piece.Color != Color)
+            {
+                validMoves.Add(position);
+            }
+            
+            break;
+        }
+        
+        return validMoves;
     }
 }
