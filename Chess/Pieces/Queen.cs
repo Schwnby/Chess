26a7a46e1263a7b0a5e0d708ed1728918680471e
Simon.Unger <Simon.Unger@digitecgalaxus.ch>
2025-08-18 14:24:14 +0200

    Implemented Queen movement

--- Chess/Pieces/Queen.cs
+++ Chess/Pieces/Queen.cs
@@ -4,6 +4,56 @@ internal sealed class Queen(PieceColor color) : Piece(color)
 {
     private protected override IEnumerable<Position> GetMoves(Position currentPosition, Board board)
     {
-        throw new NotImplementedException();
+        return [..GetRookMoves(currentPosition, board), ..GetBishopMoves(currentPosition, board)];
+    }
+
+    private List<Position> GetBishopMoves(Position currentPosition, Board board)
+    {
+        var movesUpLeft = Something(-1, -1, currentPosition, board);
+        var movesUpRight = Something(1, -1, currentPosition, board);
+        var movesDownLeft = Something(-1, 1, currentPosition, board);
+        var movesDownRight = Something(1, 1, currentPosition, board);
+
+        return [..movesUpLeft, ..movesUpRight, ..movesDownLeft, ..movesDownRight];
+    }
+
+    private List<Position> GetRookMoves(Position currentPosition, Board board)
+    {
+        var movesUp = Something(0, 1, currentPosition, board);
+        var movesRight = Something(1, 0, currentPosition, board);
+        var movesDown = Something(0, -1, currentPosition, board);
+        var movesLeft = Something(-1, 0, currentPosition, board);
+        
+        return [..movesUp, ..movesDown, ..movesLeft, ..movesRight];
+    } 
+    
+    private List<Position> Something(int rankOffset, int fileOffset, Position position, Board board)
+    {
+        var validMoves = new List<Position>();
+        
+        while (true)
+        {
+            position = new Position(position.Rank + rankOffset, position.File + fileOffset);
+            if (position.IsInbound() is false)
+            {
+                break;
+            }
+            
+            var piece = board.GetPiece(position);
+            if (piece is null)
+            {
+                validMoves.Add(position);
+                continue;
+            }
+
+            if (piece.Color != Color)
+            {
+                validMoves.Add(position);
+            }
+            
+            break;
+        }
+        
+        return validMoves;
     }
 }
