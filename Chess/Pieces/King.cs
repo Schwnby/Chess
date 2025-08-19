4b9b1300d5bcfbe02f29ee8877ca687c3bcbf06e
Schwaneby <simonunger@bluewin.ch>
2025-08-19 18:28:52 +0200

    Implemented King movement

--- Chess/Pieces/King.cs
+++ Chess/Pieces/King.cs
@@ -4,6 +4,36 @@ internal sealed class King(PieceColor color) : Piece(color)
 {
     private protected override IEnumerable<Position> GetMoves(Position currentPosition, Board board)
     {
-        throw new NotImplementedException();
+        var validMoves = new List<Position?>
+        {
+            GetPotentialMove(-1, -1, currentPosition, board),
+            GetPotentialMove(-1, 0, currentPosition, board), 
+            GetPotentialMove(-1, 1, currentPosition, board),
+            GetPotentialMove(0, 1, currentPosition, board),
+            GetPotentialMove(1, 1, currentPosition, board),
+            GetPotentialMove(1, 0, currentPosition, board),
+            GetPotentialMove(1, -1, currentPosition, board),
+            GetPotentialMove(0, -1, currentPosition, board),
+        };
+
+        return validMoves.OfType<Position>();
+    }
+    
+    private Position? GetPotentialMove(int rankOffset, int fileOffset, Position position, Board board)
+    {
+        position = new Position(position.Rank + rankOffset, position.File + fileOffset);
+
+        if (position.IsInbound() is false)
+        {
+            return null;
+        }
+        
+        var piece = board.GetPiece(position);
+        if (piece is not null && piece.Color == Color)
+        { 
+            return null;
+        }
+
+        return position;
     }
 }
