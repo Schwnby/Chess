94d6002f5e1ec5d759c46cee5636e6e460579f55
Schwaneby <simonunger@bluewin.ch>
2025-08-19 18:42:33 +0200

    Readded PieceType

--- Chess/PieceColor.cs
+++
@@ -1,7 +0,0 @@
-namespace Chess;
-
-public enum PieceColor
-{
-    White,
-    Black
-}
---
+++ Chess/PieceType.cs
@@ -0,0 +1,17 @@
+namespace Chess;
+
+public enum PieceType
+{
+    Bishop,
+    King,
+    Knight,
+    Pawn,
+    Queen,
+    Rook,
+}
+
+public enum PieceColor
+{
+    White,
+    Black
+}
